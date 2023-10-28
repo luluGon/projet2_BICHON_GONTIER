52801b6cc8ee86e8ab8fd744a8ea66c9a6172cf9
Lucien GONTIER <lucien.gontier@etu.univ-nantes.fr>
2023-10-28 08:47:36 +0000

    Upload New File

---
+++ headers/constante.h
@@ -0,0 +1,15 @@
+/*Header servant a définir les constantes */
+
+#ifndef constante
+#define constante
+  
+  #define pi 3.141592653589793 
+  #define H 1.0 
+  #define L 1.0
+  #define N 10 //Le nombre de poids et points pour Gauss_Legendre //
+  
+  
+
+  
+  
+#endif
