c9df985d426cd428fcf5d6d89923d56f21e8b91b
PierreBch <150552519+PierreBch@users.noreply.github.com>
2023-11-15 17:37:53 +0100

    Add files via upload

---
+++ headers/ProduitMatrice.h
@@ -0,0 +1,11 @@
+//Dans ce Headers, on définie les fonctions utilisées pour l'inversion de matrice dans la méthode des noyaux séparables
+#ifndef fonctions
+#define fonctions
+
+    #include constante.h
+    
+    
+    double prod_mat(double A[N1+1,N1+1],double B[N1+1])
+
+
+#endif
